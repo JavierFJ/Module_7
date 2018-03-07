f5cbc61b630ef38f2a0b3ce86fa127ac846a91e2
Francisco Javier <33847307+JavierFJ@users.noreply.github.com>
2018-03-07 21:53:23 +0100

    Main code for the chart

--- Advanced_task/main2.js
+++ Advanced_task/main2.js
@@ -1,4 +1,5 @@
-    var w = window.innerWidth,
+//Declaration of variables and defintion of the canvas.   
+var w = window.innerWidth,
           h = window.innerHeight,
           margin = { top: 40, right: 80, bottom: 200, left: 100 },
           radius = 8;
@@ -8,13 +9,13 @@
         height: h
       });
 
-      // We're passing in a function in d3.max to tell it what we're maxing (x value)
+      // We're passing in a function in d3.max to tell it what we're maxing (x value), in this case Year variable.
       var xScale = d3.scale.linear()                
           .domain([1997, d3.max(malaga, function (d) { return d.Year; })])
           .range([margin.left, w - margin.right]);  // Set margins for x specific
 
-      // We're passing in a function in d3.max to tell it what we're maxing (y value)
-      var yScale = d3.scale.linear()               //return d.Value
+      // We're passing in a function in d3.max to tell it what we're maxing (y value), in this case Value variable.
+      var yScale = d3.scale.linear()               
           .domain([d3.max(malaga, function (d) { return d.Value + 10; }),528000])
           .range([margin.top, h - margin.bottom]);  // Set margins for y specific
 
@@ -22,8 +23,7 @@
       var xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(d3.format(''));               
       var yAxis = d3.svg.axis().scale(yScale).orient("left").tickFormat(d3.format(''));
     
-        // Adds X-Axis as a 'g' element
-      
+       // Adds X-Axis as a 'g' element, as well as the label of the axis
       svg.append("g").attr({
         "class": "axis",  // Give class so we can style it
         transform: "translate(" + [0, margin.bottom+213] + ")"  // Translate just moves it down into position (or will be on top)
@@ -37,7 +37,7 @@
       .text("Year");
 
     
-      // Adds Y-Axis as a 'g' element
+      // Adds Y-Axis as a 'g' element, as well as the label of the axis.
       svg.append("g").attr({
         "class": "axis",
         transform: "translate(" + [margin.left, 0] + ")"
@@ -49,11 +49,15 @@
        .attr("text-anchor", "end")
        .text("Population");
     
-       var circleAttrs = {              //d.Year y d.Value
+      
+    // Definition of the circles with position according to our data values.
+    var circleAttrs = {           
         cx: function(d) { return xScale(d.Year); },
         cy: function(d) { return yScale(d.Value); },
         r: radius
     };   
+
+     // Select that all circles have the attributes of interactivity (MouseOver,MouseOut, on "click").
        svg.selectAll("circle")
           .data(malaga)
           .enter()
@@ -75,7 +79,7 @@
             // Specify where to put label of text
             svg.append("text").attr({
                id: "t" + d.Year + "-" + d.Value + "-" + i,  // Create an id for text so we can select it later for removing on mouseout
-                x: function() { return xScale(d.Year) - 30; }, //d.Value
+                x: function() { return xScale(d.Year) - 30; }, 
                 y: function() { return yScale(d.Value) - 15; }
             })
             .text(function() {
